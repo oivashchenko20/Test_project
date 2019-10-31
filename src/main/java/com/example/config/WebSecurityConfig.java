a879c932bc2e915add46d84b3d05f00a4e617761
sowcraft <sashka.ivasschenko@gmail.com>
2019-10-31 21:18:39 +0200

    first

--- src/main/java/com/example/config/WebSecurityConfig.java
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -31,10 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder(8);
-    }
+   
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
